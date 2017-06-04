98c0c4872e5a2c189b3484485ba4d95fd3dc845a
nel215 <otomo.yuhei@gmail.com>
2017-06-04 15:55:50 +0900

    Add cross validation

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import dogs_vs_cats.model
 import argparse
 import cupy
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
 from chainer import Variable
+import chainer
 import chainer.links as L
 import chainer.functions as F
 from chainer.optimizers import SGD
@@ -33,15 +35,26 @@ if __name__ == '__main__':
 
     images = np.array(
         list(map(L.model.vision.vgg.prepare, images)), dtype=np.float32)
-    n = len(images)
+
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.05, random_state=215)
+    n = len(X_train)
 
     for epoch in range(100):
+        chainer.using_config('train', True)
         perm = np.random.permutation(n)[:48]
-        X = Variable(xp.array(images[perm]))
-        y = Variable(xp.array(labels[perm]))
+        X = Variable(xp.array(X_train[perm]))
+        y = Variable(xp.array(y_train[perm]))
         model.cleargrads()
         pred = model(X)
         loss = F.sigmoid_cross_entropy(pred, y)
-        print(loss.data)
+        print("train loss:", loss.data)
         loss.backward()
         optimizer.update()
+
+        # validation
+        chainer.using_config('train', False)
+        X = Variable(xp.array(X_test))
+        y = Variable(xp.array(y_test))
+        pred = model(X)
+        loss = F.sigmoid_cross_entropy(pred, y)
+        print("test loss:", loss.data)
