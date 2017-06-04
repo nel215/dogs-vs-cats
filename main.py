bcc049a7ba32ff132ed3c55f2f22aaf24fa25ebd
nel215 <otomo.yuhei@gmail.com>
2017-06-04 20:40:44 +0900

    Delete unused import

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 # coding: utf-8
 import numpy as np
 import dogs_vs_cats
-import dogs_vs_cats.model
 import argparse
-import cupy
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from chainer import Variable
-import chainer
-import chainer.links as L
-import chainer.functions as F
-from chainer.optimizers import SGD
 
 
 if __name__ == '__main__':
