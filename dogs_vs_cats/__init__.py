1e101fea5a819cb69090885fd46dd398aa6b6f79
nel215 <otomo.yuhei@gmail.com>
2017-06-04 19:15:25 +0900

    Refactor training of vgg16

--- dogs_vs_cats/__init__.py
+++ dogs_vs_cats/__init__.py
@@ -1,7 +1,16 @@
 # coding: utf-8
 import os
 import numpy as np
+import cupy
+import chainer
+import chainer.links as L
+import chainer.functions as F
+import dogs_vs_cats.model
 from PIL import Image
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from chainer import Variable
+from chainer.optimizers import SGD
 
 
 def load_images(img_dir):
@@ -19,3 +28,46 @@ def load_images(img_dir):
         labels.append(label)
 
     return images, labels
+
+
+def train_vgg16(gpu):
+    if gpu is not None:
+        xp = cupy
+        model = dogs_vs_cats.model.VGG16().to_gpu()
+    else:
+        xp = np
+        model = dogs_vs_cats.model.VGG16()
+
+    optimizer = SGD(lr=0.001)
+    optimizer.setup(model)
+
+    images, labels = dogs_vs_cats.load_images('./dataset/train/')
+    label_binarizer = LabelBinarizer()
+    labels = label_binarizer.fit_transform(labels)
+    labels = labels.astype(np.int32)
+
+    images = np.array(
+        list(map(L.model.vision.vgg.prepare, images)), dtype=np.float32)
+
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.05, random_state=215)
+    n = len(X_train)
+
+    for epoch in range(100):
+        chainer.using_config('train', True)
+        perm = np.random.permutation(n)[:48]
+        X = Variable(xp.array(X_train[perm]))
+        y = Variable(xp.array(y_train[perm]))
+        model.cleargrads()
+        pred = model(X)
+        loss = F.sigmoid_cross_entropy(pred, y)
+        print("train loss:", loss.data)
+        loss.backward()
+        optimizer.update()
+
+        # validation
+        chainer.using_config('train', False)
+        X = Variable(xp.array(X_test))
+        y = Variable(xp.array(y_test))
+        pred = model(X)
+        loss = F.sigmoid_cross_entropy(pred, y)
+        print("test loss:", loss.data)
--- main.py
+++ main.py
@@ -18,43 +18,4 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', help='the id of gpu', type=int)
     args = parser.parse_args()
 
-    if args.gpu is not None:
-        xp = cupy
-        model = dogs_vs_cats.model.VGG16().to_gpu()
-    else:
-        xp = np
-        model = dogs_vs_cats.model.VGG16()
-
-    optimizer = SGD(lr=0.001)
-    optimizer.setup(model)
-
-    images, labels = dogs_vs_cats.load_images('./dataset/train/')
-    label_binarizer = LabelBinarizer()
-    labels = label_binarizer.fit_transform(labels)
-    labels = labels.astype(np.int32)
-
-    images = np.array(
-        list(map(L.model.vision.vgg.prepare, images)), dtype=np.float32)
-
-    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.05, random_state=215)
-    n = len(X_train)
-
-    for epoch in range(100):
-        chainer.using_config('train', True)
-        perm = np.random.permutation(n)[:48]
-        X = Variable(xp.array(X_train[perm]))
-        y = Variable(xp.array(y_train[perm]))
-        model.cleargrads()
-        pred = model(X)
-        loss = F.sigmoid_cross_entropy(pred, y)
-        print("train loss:", loss.data)
-        loss.backward()
-        optimizer.update()
-
-        # validation
-        chainer.using_config('train', False)
-        X = Variable(xp.array(X_test))
-        y = Variable(xp.array(y_test))
-        pred = model(X)
-        loss = F.sigmoid_cross_entropy(pred, y)
-        print("test loss:", loss.data)
+    dogs_vs_cats.train_vgg16(args.gpu)
