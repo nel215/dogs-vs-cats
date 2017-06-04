f9de9e8a407acf130cb24bb8005672936b24ca15
nel215 <otomo.yuhei@gmail.com>
2017-06-04 15:04:21 +0900

    Enable to use gpu

--- main.py
+++ main.py
@@ -2,14 +2,25 @@
 import numpy as np
 import dogs_vs_cats
 import dogs_vs_cats.model
+import argparse
+import cupy
 from chainer import Variable
 import chainer.links as L
 
 
 if __name__ == '__main__':
-    xp = np
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', help='the id of gpu', type=int)
+    args = parser.parse_args()
+
+    if args.gpu is not None:
+        xp = cupy
+        model = dogs_vs_cats.model.VGG16().to_gpu()
+    else:
+        xp = np
+        model = dogs_vs_cats.model.VGG16()
+
     images, labels = dogs_vs_cats.load_images('./dataset/train/')
-    model = dogs_vs_cats.model.VGG16()
 
     X = Variable(xp.array(
         list(map(L.model.vision.vgg.prepare, images)), dtype=xp.float32))
