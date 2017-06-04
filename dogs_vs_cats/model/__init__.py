6a1f01c4b99d78f4b2917fc253671ad1639821b0
nel215 <otomo.yuhei@gmail.com>
2017-06-04 14:55:25 +0900

    Refactor model module

---
+++ dogs_vs_cats/model/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import chainer.links as L
+from chainer import Chain
+
+
+class VGG16(Chain):
+
+    def __init__(self):
+        super(VGG16, self).__init__(
+            vgg16=L.VGG16Layers(),
+            fc8=L.Linear(4096, 2),
+        )
+
+    def __call__(self, x):
+        h1 = self.vgg16(x, layers=['fc7'])['fc7']
+        h2 = self.fc8(h1)
+        return h2
